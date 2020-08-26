98bf129860711b87cf8a5409dbeac7afa6562bdf
Kris Molinari <thefullstackguy@gmail.com>
2020-08-26 14:51:10 -0500

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from os import envrion
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse(f'<h1>{environ.get("TEST", "Nope")}</h1>')
